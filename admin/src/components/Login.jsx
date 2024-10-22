1f0d397b161996b328eb1e280fccb18ba2647c3f
Suganth <suganthchandran07@gmail.com>
2024-10-22 21:58:54 +0530

    updated

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 
 const Login = () => {
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('')
+
+    const handleSubmit =  async (e) => {
+        try {
+            e.preventDefault();
+            console.log(email)
+            console.log(password)
+        }
+        catch(error) {
+
+        }
+    }
+
   return (
     <div className='login'>
         <div className='login-container'>
             <h1 className='login-head'>Admin Panel</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='login-element-container'>
                     <p className='login-label'>Email</p>
-                    <input className='login-input' type='email' placeholder='your@email.com' required />
+                    <input onChange={(e)=> setEmail(e.target.value)} value={email} className='login-input' type='email' placeholder='your@email.com' required />
                 </div>
                 <div className='login-element-container'>
                     <p className='login-label'>Password</p>
-                    <input className='login-input' type='password' placeholder='Enter your password' required />
+                    <input onChange={(e)=> setPassword(e.target.value)} value={password} className='login-input' type='password' placeholder='Enter your password' required />
                 </div>
                 <div className='login-button-container'>
                 <button className='login-button' type='submit'>Login</button>
